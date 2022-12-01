a7ed380d06b2752f2ebcb81200a1dce78ad45459
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-11-30 21:06:39 -0300

    Datatypes

--- build/js/datatypes.js
+++ build/js/datatypes.js
@@ -1,14 +1,22 @@
 "use strict";
-// let fname = 'test';
-// fname = 'lucas';
-// console.log(fname);
-// let lname : string;
-// lname = "Lucas"
-// const newName = lname.toUpperCase();
-// console.log(newName)
-let age;
-age = 32;
-let bob = "23";
-let result = parseInt(bob);
-console.log(age);
-console.log('result -> ', result);
+// // let fname = 'test';
+// // fname = 'lucas';
+// // console.log(fname);
+// // let lname : string;
+// // lname = "Lucas"
+// // const newName = lname.toUpperCase();
+// // console.log(newName)
+// let age: number;
+// age = 32
+// let bob = "23";
+// let result = parseInt(bob)
+// console.log(age)
+// console.log('result -> ', result);
+// let valid : boolean = false
+// console.log(valid)
+let numList;
+numList = [1, 2, 3, 4, 5];
+let result = numList.filter((num) => num > 2);
+let sumAll = numList.reduce((acc, sum) => acc + sum);
+console.log(result);
+console.log('Soma de todos os valores ', sumAll);
--- datatypes.ts
+++ datatypes.ts
@@ -1,20 +1,35 @@
-// let fname = 'test';
-// fname = 'lucas';
+// // let fname = 'test';
+// // fname = 'lucas';
 
-// console.log(fname);
+// // console.log(fname);
 
-// let lname : string;
-// lname = "Lucas"
-// const newName = lname.toUpperCase();
+// // let lname : string;
+// // lname = "Lucas"
+// // const newName = lname.toUpperCase();
 
-// console.log(newName)
+// // console.log(newName)
 
-let age: number;
-age = 32
+// let age: number;
+// age = 32
 
-let bob = "23";
+// let bob = "23";
 
 
-let result = parseInt(bob)
-console.log(age)
-console.log('result -> ', result);
+// let result = parseInt(bob)
+// console.log(age)
+// console.log('result -> ', result);
+
+// let valid : boolean = false
+// console.log(valid)
+
+let numList : Array<number>;
+
+numList = [1, 2, 3, 4, 5]
+
+let result = numList.filter((num) => num > 2)
+
+let sumAll = numList.reduce((acc, sum) => acc + sum)
+
+console.log(result)
+
+console.log('Soma de todos os valores ', sumAll)
