be15f5a7b837e7e0e88d1bf450f8b06b3bb953a5
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-12-29 21:20:03 -0300

    Criando o primeiro método da classe Employee

--- build/js/classes.js
+++ build/js/classes.js
@@ -2,10 +2,13 @@
 /* Classes */
 class Employee {
     constructor(id, name, address) {
-        this.id = id,
-            this.name = name,
-            this.address = address;
+        (this.id = id), (this.name = name), (this.address = address);
+    }
+    getNameWithAddres() {
+        return `${this.name} ${this.address}`;
     }
 }
-let john = new Employee(1, 'lucas', 'Rua Ferreiar de Oliveia 74');
+let john = new Employee(1, "lucas", "Rua Ferreiar de Oliveia 74");
+let address = john.getNameWithAddres();
 console.log(john);
+console.log(address);
--- classes.ts
+++ classes.ts
@@ -1,17 +1,21 @@
 /* Classes */
 
 class Employee {
-    id: number;
-    name: string;
-    address: string;
+  id: number;
+  name: string;
+  address: string;
 
-    constructor(id: number, name: string, address: string) {
-        this.id = id,
-        this.name = name,
-        this.address = address
-    }
+  constructor(id: number, name: string, address: string) {
+    (this.id = id), (this.name = name), (this.address = address);
+  }
+
+  getNameWithAddres(): string {
+    return `${this.name} ${this.address}`
+  }
 }
 
-let john = new Employee(1, 'lucas', 'Rua Ferreiar de Oliveia 74');
+let john = new Employee(1, "lucas", "Rua Ferreiar de Oliveia 74");
+let address = john.getNameWithAddres();
 
 console.log(john);
+console.log(address)
