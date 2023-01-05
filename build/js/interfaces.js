cfd93610817b0de4741de023be82ef84ee7a3e89
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-05 20:47:57 -0300

    Creating an object with interface

--- build/js/interfaces.js
+++ build/js/interfaces.js
@@ -1,2 +1,4 @@
 "use strict";
 // Creating interfaces
+let user = { name: "Lucas", id: 9, email: "lucasneves17@outlook.com" };
+console.log(user);
--- interfaces.ts
+++ interfaces.ts
@@ -2,8 +2,15 @@
 
 interface User {
   name: string;
-  age: number;
+  age?: number;
   id: number;
   email: string;
 }
 
+let user: User = { name: "Lucas", id: 9, email: "lucasneves17@outlook.com"}
+
+interface Employeer extends User {
+  salary: number
+}
+
+let employee : Employeer = {name: "Lucas", id: 2, email: "luc@g.com", salary: 2000 }
